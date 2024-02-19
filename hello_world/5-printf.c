22e6d048ed1d52310ffd8fd42cb2bd61bd1e2104
Maxime3003 <martinsmaxime3@gmail.com>
2024-02-19 13:45:31 +0000

    adding program C in file 5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - entry point 
- * Description: inclure un texte dans le programme avec la commande printf
+ * main - Entry point 
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
