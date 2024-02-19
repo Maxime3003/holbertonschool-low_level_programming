4fdb6165b58d8eb35893d9682461e56496f6440f
Maxime3003 <martinsmaxime3@gmail.com>
2024-02-19 18:12:12 +0000

    adding file 1-last_digit.c

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of");
+	printf("n");
+	printf("is");
+
+	if (n > 5)
+	printf("%d and is greater than 5\n", n);
+	else if (n = 0)
+	printf("and is 0\n", n);
+	else
+	printf("%d and is less than 6 and not 0\n", n);	
+	return (0);
+}
