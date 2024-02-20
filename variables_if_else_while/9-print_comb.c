8cd391bc92c49b519ce821bebdf0e2cdff97cc35
Maxime3003 <martinsmaxime3@gmail.com>
2024-02-20 15:00:20 +0000

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+{
+		if (i <= 8)
+{
+		putchar('0' + i);
+		putchar(44);
+		putchar(32);
+}
+		else
+		putchar('0' + i);
+}
+	return (0);
+}
