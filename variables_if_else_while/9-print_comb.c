6e2bdc0acc31fa59beae4bfa0602e49edd34e438
Maxime3003 <martinsmaxime3@gmail.com>
2024-02-20 15:36:00 +0000

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,15 +9,17 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
+	{
 		putchar('0' + i);
-{
-		if (i <= 8)
-{
-		putchar(44);
-		putchar(32);
-}
-		putchar('\n');
-}
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	
+	}
+	putchar('\n');
+	
 	return (0);
 }
