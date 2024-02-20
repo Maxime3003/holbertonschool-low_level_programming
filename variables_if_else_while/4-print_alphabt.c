5e5f8b8ad7952de5c1ed1e5a86a6b013c46dd5be
Maxime3003 <martinsmaxime3@gmail.com>
2024-02-20 08:49:23 +0000

    adding file 4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char letter;
+for (letter = 'a' ; letter <= 'z' ; letter++)
+if (letter != 'q' && 'e')
+putchar (letter);
+putchar ('\n');
+return (0);
+}
