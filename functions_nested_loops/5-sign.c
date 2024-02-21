1c7848de20f04ffa1f5a431d0d1ebaf260bcbb71
Maxime3003 <martinsmaxime3@gmail.com>
2024-02-21 18:09:59 +0000

    adding program 5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,19 +2,19 @@
 
 /**
  * print_sign - prints the sign of a number
- * @c: - integer value
- * Return: 1 if n is greater than 0, 0 is n is equal to 0 and -1 if n is less than 0
+ * @n: - integer value
+ * Return: 1 if n greater 0, 0 if n equal to 0, -1 if n less than 0
  */
 
 int print_sign(int n)
 
 {
-	if (n >= 49 && n <= 57)
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-		else if (n == 48)
+		else if (n == 0)
 		{
 			_putchar(48);
 			return (0);
