a082acfbd36ad892fe62740acde934a447d8cdba
Maxime3003 <martinsmaxime3@gmail.com>
2024-02-21 17:26:08 +0000

    adding program 3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * int _islower(int c) - checks the lowercase
- * Return: void
+ * _islower - checks the lowercase
+ * @c: - integer value
+ * Return: 1 if c is lowercase, else 0
  */
 
 int _islower(int c)
 
 {
-	if(c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	return (1);
 		else
 		return (0);
